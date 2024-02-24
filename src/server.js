04f84fa4ec059aec1013fef75722407f53960a6d
kobster68 <kbl1362@g.rit.edu>
2024-02-24 14:03:33 -0500

    added basic functionality to htmlResponses (web page loads)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 const http = require('http');
+const url = require('url');
+
+const htmlHandler = require('./htmlResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const onRequest = () => {};
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (parsedUrl.pathname === '/') {
+    htmlHandler.getIndex(request, response);
+  }
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  }
+};
 
 http.createServer(onRequest).listen(port);
 console.log(`Listening on 127.0.0.1: ${port}`);
